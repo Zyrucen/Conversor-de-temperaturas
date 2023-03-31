1de66fc4ca9dcef27fcd316fa5a513877f6d9de2
Zyrucen <116550262+Zyrucen@users.noreply.github.com>
2023-03-31 11:45:51 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Conversor_de_temperaturas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
